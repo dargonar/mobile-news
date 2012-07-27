1a003fa769946045735988bcdd63eab169d3851b
Pablo <ptutino@gmail.com>
2012-07-27 13:42:07 -0300

    cosillas

--- web/appengine/apps/frontend/urls.py
+++ web/appengine/apps/frontend/urls.py
@@ -10,7 +10,6 @@ def get_rules():
         Route('/fullversion',             name='frontend/home/fullversion',   handler='apps.frontend.home.Index:fullversion'),
         Route('/view/<article>',          name='frontend/article',            handler='apps.frontend.home.ViewArticle'),
         Route('/m/secciones',             name='frontend/secciones',          handler='apps.frontend.home.ListSecciones'),
-        #Route('/m/secciones/<category>',  name='frontend/secciones/category', handler='apps.frontend.home.SeccionArticles'),
         Route('/m/secciones/<category>',  name='frontend/secciones/category', handler='apps.frontend.home.Index:get_seccion_articles'),
         Route('/m/servicios',             name='frontend/servicios',          handler='apps.frontend.home.ListServicios'),
         Route('/m/clasificados',          name='frontend/clasificados',       handler='apps.frontend.home.ListClasificados'),
@@ -18,6 +17,14 @@ def get_rules():
         Route('/m/suplementos',           name='frontend/suplementos',        handler='apps.frontend.home.ListSuplementos'),
         Route('/m/perfil',                name='frontend/perfil',             handler='apps.frontend.home.Profile'),
         Route('/diarios/csv',             name='frontend/csv',                handler='apps.frontend.home.DiariosCsv'),
+      ]),
+      PathPrefixRoute('/demo/service2', [
+        Route('/2/',                        name='frontend2/home',               handler='apps.frontend.handlers.Index'),
+        Route('/2/fullversion',             name='frontend2/home/fullversion',   handler='apps.frontend.handlers.Index:fullversion'),
+        Route('/2/view/<article>',          name='frontend2/article',            handler='apps.frontend.handlers.ViewArticle'),
+        Route('/2/m/secciones',             name='frontend2/secciones',          handler='apps.frontend.handlers.ListSecciones'),
+        Route('/2/m/secciones/<category>',  name='frontend2/secciones/category', handler='apps.frontend.handlers.Index:get_seccion_articles'),
+        
       ])
     ]
     
