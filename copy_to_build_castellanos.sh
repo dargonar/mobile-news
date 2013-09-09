7fea67fadb0f1d12c786252c81e5b7e3b1bcf8a4
Marku <matias.romeo@gmail.com>
2013-09-09 19:18:08 -0300

    distribuyendolo

--- copy_to_build_castellanos.sh
+++ copy_to_build_castellanos.sh
@@ -1,4 +1,4 @@
 cp ElDia/images/castellanos_new/*.png ElDia/images/
-cd ElDia
-xcodebuild -scheme Castellanos -target Castellanos archive
-cd ..
+# cd ElDia
+# xcodebuild -scheme Castellanos -target Castellanos archive
+# cd ..
