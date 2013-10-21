3101a64be9ed875e6796bee960e7b8276faf44f7
Pablo <ptutino@gmail.com>
2013-10-21 11:34:50 -0300

    Archivos faltantes para builder la reforma app.

---
+++ copy_to_build_lareforma.sh
@@ -0,0 +1,6 @@
+cp ElDia/images/lareforma/*.png ElDia/images/
+cp ElDia/images/lareforma/Icon~ipad.png ElDia/Icon~ipad.png
+cp ElDia/images/lareforma/Icon.png ElDia/Icon.png
+# cd ElDia
+# xcodebuild -scheme ElDia -target ElDia archive
+# cd ..
