0214275c5773abcf32eed886acdfc120d8833b79
Pablo <ptutino@gmail.com>
2013-11-05 22:09:44 -0300

    Agregue el copyator.

---
+++ copy_to_build_elnorte.sh
@@ -0,0 +1,6 @@
+cp ElDia/images/elnorte/*.png ElDia/images/
+cp ElDia/images/elnorte/Icon~ipad.png ElDia/Icon~ipad.png
+cp ElDia/images/elnorte/Icon.png ElDia/Icon.png
+# cd ElDia
+# xcodebuild -scheme ElDia -target ElDia archive
+# cd ..
