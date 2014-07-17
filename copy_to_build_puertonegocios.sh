a67fad8b0d1a49690b66fb7bad3027e453eaffc7
Pablo <ptutino@gmail.com>
2014-07-17 10:23:25 -0300

    Puerto Negocios ObjectiveC files.

---
+++ copy_to_build_puertonegocios.sh
@@ -0,0 +1,6 @@
+cp ElDia/images/puertonegocios/*.png ElDia/images/
+cp ElDia/images/puertonegocios/Icon~ipad.png ElDia/Icon~ipad.png
+cp ElDia/images/puertonegocios/Icon.png ElDia/Icon.png
+# cd ElDia
+# xcodebuild -scheme ElDia -target ElDia archive
+# cd ..
